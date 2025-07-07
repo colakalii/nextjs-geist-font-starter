fdffba1c0b94a1ca12fb547864b23fdeebea1fcb
Ali ÇOLAK <144851886+colakalii@users.noreply.github.com>
2025-07-07 12:02:50 +0300

    Add .next/static/chunks/src_app_page_tsx_327a9e50._.js 1751878969896

---
+++ .next/static/chunks/src_app_page_tsx_327a9e50._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_7ec1abdd._.js",
+  "static/chunks/node_modules_d10398d6._.js"
+],
+    source: "dynamic"
+});
